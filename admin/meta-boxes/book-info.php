d9ad98b63f33bfda43718cc5af979f4a1c7f8bf5
ngeneva <ngeneva@htmlbox.com>
2022-05-29 09:12:15 +0300

    Convert space indentation to tab

--- admin/meta-boxes/book-info.php
+++ admin/meta-boxes/book-info.php
@@ -6,20 +6,20 @@ $description_value = get_post_meta( $post->ID, '_bc_book_description', true );
 
 <div class="section-book-crud-author">
 	<div class="section__body">
-	    <p class="meta-options">
-	        <label for="bc-author-first-name"><?php _e( 'First Name', 'books-crud' ); ?></label>
-	        <input id="bc-author-first-name" type="text" name="bc-author-first-name" value="<?php echo $first_name_value ?>">
-	    </p>
+		<p class="meta-options">
+			<label for="bc-author-first-name"><?php _e( 'First Name', 'books-crud' ); ?></label>
+			<input id="bc-author-first-name" type="text" name="bc-author-first-name" value="<?php echo $first_name_value ?>">
+		</p>
 
-	    <p class="meta-options">
-	        <label for="bc-author-last-name"><?php _e( 'Last Name', 'books-crud' ); ?></label>
-	        <input id="bc-author-last-name" type="text" name="bc-author-last-name" value="<?php echo $last_name_value ?>">
-	    </p>
+		<p class="meta-options">
+			<label for="bc-author-last-name"><?php _e( 'Last Name', 'books-crud' ); ?></label>
+			<input id="bc-author-last-name" type="text" name="bc-author-last-name" value="<?php echo $last_name_value ?>">
+		</p>
 
-	    <p class="meta-options">
-	        <label for="bc-book-description"><?php _e( 'Description', 'books-crud' ); ?></label>
+		<p class="meta-options">
+			<label for="bc-book-description"><?php _e( 'Description', 'books-crud' ); ?></label>
 
-	        <textarea id="bc-book-description" name="bc-book-description" rows="4" cols="50"><?php echo $description_value ?></textarea>
-	    </p>
+			<textarea id="bc-book-description" name="bc-book-description" rows="4" cols="50"><?php echo $description_value ?></textarea>
+		</p>
 	</div><!-- /.section__body -->
 </div>
